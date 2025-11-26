0d0f87f8c7c14fe4fa8ca8ed52da76c06c6454b8
Saurav Kumar Verma <sauravkumarv22@gmail.com>
2025-11-26 23:59:38 +0530

    Video call featured added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import  express from 'express';
 import "dotenv/config"
 import router from './routes/auth.route.js';
+import path from "path";
 import { connectDB } from './lib/db.js';
 import cookieParser from "cookie-parser"
 import userRouter from './routes/users.route.js';
@@ -8,7 +9,9 @@ import chatRouter from './routes/chat.route.js';
 import cors from 'cors'
 
 
+
 const port=process.env.PORT|| 5000;
+const __dirname=path.resolve();
 
 const app=express();
 app.use(cors({
@@ -26,7 +29,13 @@ app.use("/api/auth",router)
 app.use("/api/users",userRouter)
 app.use('/api/chat',chatRouter)
 
+if(process.env.NODE_ENV ==="production"){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+})
+}
 
 app.listen(port,()=>{
   console.log(`http://localhost:${port}`)
